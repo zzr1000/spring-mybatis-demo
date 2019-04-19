1fda2b823317752d0f88877c6d41eeff1e0b56a2
zzr1000 <zzr1000@gmail.com>
2019-04-19 15:56:22 +0800

    JdbcTest

---
+++ src/main/java/org/zzr1000/jdbcTest/JdbcTest.java
@@ -0,0 +1,38 @@
+package org.zzr1000.jdbcTest;
+
+import java.sql.*;
+
+public class JdbcTest {
+
+    public static void main(String[] args) throws SQLException {
+        Connection conn = null;
+        String sql ;
+
+        String conn_str = "jdbc:mysql://172.172.178.75:3306/t?"
+                + "user=root&password=Root@123&useUnicode=true&characterEncoding=UTF8";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");// 动态加载mysql驱动
+            System.out.println("成功加载MySQL驱动程序");
+
+            // 一个Connection代表一个数据库连接
+            conn = DriverManager.getConnection(conn_str);
+            Statement stmt = conn.createStatement();
+            sql = "show tables;";
+            ResultSet result = stmt.executeQuery(sql);
+            if (result != null) {
+                while (result.next()) {
+                    System.out.println(result.getString(1) + "\t");
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("MySQL操作错误");
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            conn.close();
+        }
+
+    }
+
+}
