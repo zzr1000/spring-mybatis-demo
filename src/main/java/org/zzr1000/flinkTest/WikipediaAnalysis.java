3e38523da7101780ac5ec07a246ebdc073727122
zzr1000 <zzr1000@gmail.com>
2019-05-15 22:47:15 +0800

    Flink

---
+++ src/main/java/org/zzr1000/flinkTest/WikipediaAnalysis.java
@@ -0,0 +1,118 @@
+package org.zzr1000.flinkTest;
+
+
+import org.apache.flink.api.common.ExecutionConfig;
+import org.apache.flink.api.common.functions.FoldFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.runtime.state.memory.MemoryStateBackend;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditEvent;
+import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditsSource;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.java.BatchTableEnvironment;
+import org.apache.flink.table.api.java.StreamTableEnvironment;
+
+public class WikipediaAnalysis {
+
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment see = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        ExecutionConfig executionConfig = see.getConfig();
+
+
+
+        DataStream<WikipediaEditEvent> edits = see.addSource(new WikipediaEditsSource());
+
+        KeyedStream<WikipediaEditEvent, String> keyedEdits = edits
+                .keyBy(new KeySelector<WikipediaEditEvent, String>() {
+                    @Override
+                    public String getKey(WikipediaEditEvent event) {
+                        return event.getUser();
+                    }
+                });
+
+        keyedEdits.print();
+
+        StreamTableEnvironment streamTableEnvironment = StreamTableEnvironment.create(see);
+
+        TableEnvironment tEnv = TableEnvironment.getTableEnvironment(see);
+
+
+        DataStream<Tuple2<String, Long>> result = keyedEdits
+                .timeWindow(Time.seconds(5))
+                .fold(new Tuple2<>("", 0L), new FoldFunction<WikipediaEditEvent, Tuple2<String, Long>>() {
+                    @Override
+                    public Tuple2<String, Long> fold(Tuple2<String, Long> acc, WikipediaEditEvent event) {
+                        acc.f0 = event.getUser();
+                        acc.f1 += event.getByteDiff();
+                        return acc;
+                    }
+                });
+
+        result.print();
+
+        see.execute("ddd");
+
+    }
+
+    // *************************************************************************
+    //     USER DATA TYPES
+    // *************************************************************************
+
+    /**
+     * Simple POJO.
+     */
+    public static class Order {
+        public Long user;
+        public String product;
+        public int amount;
+
+        public Order() {
+        }
+
+        public Order(Long user, String product, int amount) {
+            this.user = user;
+            this.product = product;
+            this.amount = amount;
+        }
+
+        @Override
+        public String toString() {
+            return "Order{" +
+                    "user=" + user +
+                    ", product='" + product + '\'' +
+                    ", amount=" + amount +
+                    '}';
+        }
+    }
+
+    public static class WC {
+        public String word;
+        public long frequency;
+
+        // public constructor to make it a Flink POJO
+        public WC() {}
+
+        public WC(String word, long frequency) {
+            this.word = word;
+            this.frequency = frequency;
+        }
+
+        @Override
+        public String toString() {
+            return "WC " + word + " " + frequency;
+        }
+    }
+
+
+}
+
+
