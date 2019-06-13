6e9c12ea778ce65cfb7df9470dfe1905b3a8696f
zzr1000 <zzr1000@gmail.com>
2019-06-13 15:10:14 +0800

    guava md5

---
+++ src/main/java/org/zzr1000/md5Test/GuavaHashing.java
@@ -0,0 +1,24 @@
+package org.zzr1000.md5Test;
+
+import com.google.common.hash.Hashing;
+import org.junit.Test;
+
+
+public class GuavaHashing {
+
+    @Test
+    public void test() {
+        String input = "abc001";
+        // 计算MD5
+        System.out.println(Hashing.md5().hashBytes(input.getBytes()).toString());
+//        // 计算sha256
+//        System.out.println(Hashing.sha256().hashBytes(input.getBytes()).toString());
+//        // 计算sha512
+//        System.out.println(Hashing.sha512().hashBytes(input.getBytes()).toString());
+//        // 计算crc32
+//        System.out.println(Hashing.crc32().hashBytes(input.getBytes()).toString());
+
+        System.out.println(Hashing.md5().hashUnencodedChars(input).toString());
+
+    }
+}
