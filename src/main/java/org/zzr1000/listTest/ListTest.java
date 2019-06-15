65ca0772a74585587d16e74ece5a550a833ae369
zzr11000 <zzr1000@gmail.com>
2019-06-15 09:06:59 +0800

    ListTest

---
+++ src/main/java/org/zzr1000/listTest/ListTest.java
@@ -0,0 +1,45 @@
+package org.zzr1000.listTest;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    //List初始化1
+    @Test
+    public void fListTest1() {
+        List<String> name = new ArrayList();
+        name.add("xxx");
+        name.add("yyy");
+        name.add("zzz");
+
+        for(String i:name){
+            System.out.println(i);
+        }
+    }
+
+    //List初始化2
+    @Test
+    public void fListTest2() {
+        List<String> name = Arrays.asList("xxx","yyy","zzz");//长度固定
+        for(String i:name){
+            System.out.println(i);
+        }
+    }
+
+    //List初始化3
+    @Test
+    public void fListTest3() {//长度不固定
+        List<String> name = new ArrayList<>(Arrays.asList("xxx","yyy","zzz"));
+        for(String i:name){
+            System.out.println(i);
+        }
+    }
+}
