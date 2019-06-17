599c3cef5ce945b32233bffd97a0f57c07e5bcfa
zzr11000 <zzr1000@gmail.com>
2019-06-18 01:55:08 +0800

    hbaseTable

--- src/main/java/org/zzr1000/prestoHbaseTest/meta/HBaseTables.java
+++ src/main/java/org/zzr1000/prestoHbaseTest/meta/HBaseTables.java
@@ -1,13 +1,17 @@
 package org.zzr1000.prestoHbaseTest.meta;
 
+import com.facebook.presto.spi.SchemaTableName;
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
 import io.airlift.log.Logger;
+import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.NamespaceDescriptor;
 import org.apache.hadoop.hbase.client.Admin;
 import org.zzr1000.prestoHbaseTest.connection.HBaseClientManager;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class HBaseTables {
@@ -20,6 +24,7 @@ public class HBaseTables {
         this.hbaseClientManager = hbaseClientManager;
     }
 
+    //1、getSchemaNames
     public Set<String> getSchemaNames() {
         NamespaceDescriptor[] namespaceDescriptors = new NamespaceDescriptor[0];
         Admin admin = null;
@@ -46,6 +51,32 @@ public class HBaseTables {
         return set;
     }
 
+    //2、getTables
+    public Map<SchemaTableName, HBaseTableHandle> getTables(Admin admin, String schema) {
+        Map<SchemaTableName, HBaseTableHandle> tables;
+        try {
+            ImmutableMap.Builder<SchemaTableName, HBaseTableHandle> tablesBuilder = ImmutableMap.builder();
+            HTableDescriptor[] descriptors = admin.listTableDescriptorsByNamespace(schema);
+            for (HTableDescriptor table : descriptors) {
+                // If the target table is in the other namespace, table.getNameAsString() will return
+                // value like 'namespace1:tableName1', so we have to remove the unnecessary namespace.
+                String tableName;
+                if (table.getNameAsString() != null && table.getNameAsString().contains(":")) {
+                    tableName = table.getNameAsString().split(":")[1];
+                } else {
+                    tableName = table.getNameAsString();
+                }
+                SchemaTableName schemaTableName = new SchemaTableName(schema, tableName);
+
+                tablesBuilder.put(schemaTableName, new HBaseTableHandle(schemaTableName));
+            }
+            tables = tablesBuilder.build();
+            return tables;
+        } catch (Exception ex) {
+            logger.error(ex, ex.getMessage());
+        }
+        return null;
+    }
 
 
 }
