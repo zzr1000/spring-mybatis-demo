ac5368e4e2ef81bcfb752a37647416a8c66d77b3
zzr1000 <zzr1000@gmail.com>
2019-06-18 17:16:57 +0800

    属性注入

--- src/main/java/org/zzr1000/guiceTest/bInjectDemo/TestInjection1.java
+++ src/main/java/org/zzr1000/guiceTest/bInjectDemo/TestInjection1.java
@@ -4,7 +4,7 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import org.zzr1000.guiceTest.aBaseDemo.Dao;
 
-//属性注入
+//属性注入：用的比较多，不需要new，只需要注解，就可以直接引用实例
 @Singleton
 public class TestInjection1 {
 
