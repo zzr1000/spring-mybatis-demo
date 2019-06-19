8d8dfdedd90c3edea74ab2f16c4213f0b7f6da7c
zzr1000 <zzr1000@gmail.com>
2019-06-19 20:44:26 +0800

    airlift

---
+++ src/main/java/org/zzr1000/airliftTest/AConfig.java
@@ -0,0 +1,57 @@
+package org.zzr1000.airliftTest;
+
+import io.airlift.configuration.Config;
+
+import javax.validation.constraints.NotNull;
+
+//如果该类中，没有任何值，会有如下报错：
+//Error: Configuration class [org.zzr1000.airliftTest.AConfig]
+//does not have any @Config annotations
+//设置NotNull之后，要保障有对应配置信息传入
+public class AConfig {
+
+    private String configA;
+    private String configB;
+    private String configC;
+    private String configD;
+
+    @NotNull
+    public String getConfigA() {
+        return configA;
+    }
+
+    @Config("configA")
+    public void setConfigA(String configA) {
+        this.configA = configA;
+    }
+
+    @NotNull
+    public String getConfigB() {
+        return configB;
+    }
+
+    @Config("configB")
+    public void setConfigB(String configB) {
+        this.configB = configB;
+    }
+
+    @NotNull
+    public String getConfigC() {
+        return configC;
+    }
+
+    @Config("configC")
+    public void setConfigC(String configC) {
+        this.configC = configC;
+    }
+
+    @NotNull
+    public String getConfigD() {
+        return configD;
+    }
+
+    @Config("configD")
+    public void setConfigD(String configD) {
+        this.configD = configD;
+    }
+}
---
+++ src/main/java/org/zzr1000/airliftTest/AirliftTest.java
@@ -0,0 +1,48 @@
+package org.zzr1000.airliftTest;
+
+import com.google.inject.Binder;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.Scopes;
+import io.airlift.bootstrap.Bootstrap;
+import io.airlift.configuration.ConfigBinder;
+import io.airlift.json.JsonModule;
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AirliftTest {
+
+    public static void main(String[] args) throws Exception {
+
+        Map<String,String> requiredConfig = new HashMap<>();
+
+        Bootstrap e = new Bootstrap(new Module[]{new JsonModule(),new AModule()});
+
+        Injector injector = e.strictConfig()
+                .doNotInitializeLogging()
+                .setRequiredConfigurationProperties(requiredConfig)
+                .initialize();
+
+        AClass aClass = injector.getInstance(AClass.class);
+
+        System.out.println(aClass.getName());
+
+    }
+}
+
+
+@Data
+class AClass{
+    private String name;
+}
+
+class AModule implements Module {
+
+    @Override
+    public void configure(Binder binder) {
+        binder.bind(AClass.class).in(Scopes.SINGLETON);
+        ConfigBinder.configBinder(binder).bindConfig(AConfig.class);
+    }
+}
