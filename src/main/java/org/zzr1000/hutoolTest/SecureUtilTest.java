4fef4e11bd0b8f5e8eb8fc261b3e801e0b5794da
zzr11000 <zzr1000@gmail.com>
2019-06-20 01:07:49 +0800

    md5Test

---
+++ src/main/java/org/zzr1000/hutoolTest/SecureUtilTest.java
@@ -0,0 +1,15 @@
+package org.zzr1000.hutoolTest;
+
+import cn.hutool.crypto.SecureUtil;
+import com.google.common.hash.Hashing;
+import org.junit.Test;
+
+public class SecureUtilTest {
+
+    @Test
+    public void test1() {
+        System.out.println(SecureUtil.md5("aaa"));
+        System.out.println(Hashing.md5().hashBytes("aaa".getBytes()).toString());
+    }
+
+}
