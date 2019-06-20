73bf36ceca0e3147ac4341040b637b3b3cf9dcef
zzr1000 <zzr1000@gmail.com>
2019-06-20 11:50:29 +0800

    refer url

--- src/main/java/org/zzr1000/guiceTest/aBaseDemo/Test.java
+++ src/main/java/org/zzr1000/guiceTest/aBaseDemo/Test.java
@@ -3,6 +3,8 @@ package org.zzr1000.guiceTest.aBaseDemo;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
+
+//refer to : https://www.cnblogs.com/huzi007/p/5797582.html
 public class Test {
     public static void main(String[] args) {
         Injector injector = Guice.createInjector(new TestModule());
