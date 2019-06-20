e2422c59d204506a084444c11ddd4943322512f8
zzr11000 <zzr1000@gmail.com>
2019-06-20 21:28:54 +0800

    getClassLoader

--- src/main/java/org/zzr1000/classLoaderTest/ClassLoaderTest.java
+++ src/main/java/org/zzr1000/classLoaderTest/ClassLoaderTest.java
@@ -2,7 +2,9 @@ package org.zzr1000.classLoaderTest;
 
 public class ClassLoaderTest {
 
-
-
-
+    public static void main(String[] args) {
+        System.out.println(ClassLoaderTest.class.getClassLoader());
+        System.out.println(ClassLoaderTest.class.getClassLoader().getParent());
+        System.out.println(ClassLoaderTest.class.getClassLoader().getParent().getParent());
+    }
 }
