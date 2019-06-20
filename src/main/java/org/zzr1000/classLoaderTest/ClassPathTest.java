49cf5a735557b2df9f55701e5a8e80cd77e45ad3
zzr11000 <zzr1000@gmail.com>
2019-06-21 01:15:44 +0800

    classpath test

---
+++ src/main/java/org/zzr1000/classLoaderTest/ClassPathTest.java
@@ -0,0 +1,11 @@
+package org.zzr1000.classLoaderTest;
+
+import org.junit.Test;
+
+public class ClassPathTest {
+
+    @Test
+    public void test(){
+        System.out.println(System.getProperty("java.class.path"));
+    }
+}
