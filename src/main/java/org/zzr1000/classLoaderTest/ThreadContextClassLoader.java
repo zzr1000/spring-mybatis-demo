9a8c57d13fe5253d2b45da955d8f2e9f39fc020b
zzr1000 <zzr1000@gmail.com>
2019-06-20 17:26:51 +0800

    get、set ThreadContextClassLoader

---
+++ src/main/java/org/zzr1000/classLoaderTest/ThreadContextClassLoader.java
@@ -0,0 +1,21 @@
+package org.zzr1000.classLoaderTest;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class ThreadContextClassLoader implements Closeable {
+
+
+    private final ClassLoader originalThreadContextClassLoader = Thread.currentThread().getContextClassLoader();
+
+    //方法的作用是：设置新的ThreadContextClassLoader
+    //最后在关闭的时候，在把原有的ThreadContextClassLoader设置还原回去
+    public ThreadContextClassLoader(ClassLoader newThreadContextClassLoader) {
+        Thread.currentThread().setContextClassLoader(newThreadContextClassLoader);
+    }
+
+    @Override
+    public void close() throws IOException {
+        Thread.currentThread().setContextClassLoader(this.originalThreadContextClassLoader);
+    }
+}
