835faf7798a984a71f9f22049948fbf8afa4f84b
zzr1000 <zzr1000@gmail.com>
2019-06-21 16:33:55 +0800

    todo

---
+++ src/main/java/org/zzr1000/todo/TODO.java
@@ -0,0 +1,12 @@
+package org.zzr1000.todo;
+
+public class TODO {
+
+    public static void main(String[] args) {
+        String todo = "greenplum";
+        todo = todo + "Generic";
+
+        System.out.println(todo);
+    }
+
+}
