27fe68222863baf68ff9ffe3e56e530a529f7da8
zzr1000 <zzr1000@gmail.com>
2019-06-21 15:41:24 +0800

    ci URLClassLoader

--- src/main/java/org/zzr1000/classLoaderTest/URLClassLoaderTest.java
+++ src/main/java/org/zzr1000/classLoaderTest/URLClassLoaderTest.java
@@ -3,7 +3,7 @@ package org.zzr1000.classLoaderTest;
 
 import org.junit.Test;
 
-import java.lang.reflect.Field;
+import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
@@ -12,11 +12,12 @@ import java.net.URLClassLoader;
 
 //URLClassLoader 获取外部的jar文件以及class文件
 //refer to : https://blog.csdn.net/qq_29956725/article/details/86677379
-//测试jar包产生：jar cvf test.jar Test.class，放在项目根目录下：..
+//测试jar包产生：jar cvf test.jar Test.class，放在项目根目录下：.. : 注意包名信息：..
 public class URLClassLoaderTest {
 
     //这样写，拿不到jar包，拿不到类：..
-    private static String pathJar = "./test.jar";
+    //private static String pathJar = "./test.jar";
+    private static String jarName = "test.jar";
 
     //区别：不需要加具体class文件
     private static String pathClass = "./";
@@ -28,8 +29,14 @@ public class URLClassLoaderTest {
             InstantiationException,
             NoSuchMethodException,
             InvocationTargetException {
+        String p1 = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        File f1 = new File(p1);
+        String s1 = new File(f1.getParent()).getParent();
+        String pathJar = s1 + "\\" + jarName;
+        System.out.println(pathJar);
         URL url = new URL("file:" + pathJar);
         URLClassLoader urlClassLoader = new URLClassLoader(new URL[] { url }); // 加载外部的jar包
+        //类名如果有完整的包名，那么jar文件中，也需要有完整的对应目录信息，否则会报找不到对应类
         Class<?> clazz = urlClassLoader.loadClass("org.zzr1000.classLoaderTest.Test");
 
         Object instance = clazz.newInstance();
---
+++ src/main/java/org/zzr1000/todo/shellTest/ShellTest.java
@@ -0,0 +1,9 @@
+package org.zzr1000.todo.shellTest;
+
+public class ShellTest {
+
+    public static void main(String[] args) {
+
+    }
+
+}
