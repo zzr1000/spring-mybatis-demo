545e45f614a7cb2aae4f2b6d75a24792aa5492b4
zzr11000 <zzr1000@gmail.com>
2019-06-26 22:53:24 +0800

    guava version

--- src/main/java/org/zzr1000/kafkaTest/ProducerFastStart.java
+++ src/main/java/org/zzr1000/kafkaTest/ProducerFastStart.java
@@ -1,7 +1,9 @@
 package org.zzr1000.kafkaTest;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.zzr1000.propertyLoadTest.PropertyLoadTest;
 
 
@@ -18,9 +20,16 @@ public class ProducerFastStart {
     public static void main(String[] args) {
 
         Properties properties = new Properties();
+
         properties.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
         properties.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
         properties.put("bootstrap.servers",brokerList);
+
+      //上述三个配置，可优化为此写法
+      //properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+      //properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+      //properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,brokerList);
+
 
         //配置生产者客户端参数并创建KafkaProducer实例
         KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties);
