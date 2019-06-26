7a4ed5f75aa7357fcf23826590b841b228c500ab
zzr1000 <zzr1000@gmail.com>
2019-06-26 15:56:42 +0800

    md5

--- src/main/java/org/zzr1000/hutoolTest/SecureUtilTest.java
+++ src/main/java/org/zzr1000/hutoolTest/SecureUtilTest.java
@@ -2,6 +2,7 @@ package org.zzr1000.hutoolTest;
 
 import cn.hutool.crypto.SecureUtil;
 import com.google.common.hash.Hashing;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
 
 public class SecureUtilTest {
@@ -9,7 +10,9 @@ public class SecureUtilTest {
     @Test
     public void test1() {
         System.out.println(SecureUtil.md5("aaa"));
-        System.out.println(SecureUtil.md5("aaa").substring(24));
+        System.out.println(DigestUtils.md5Hex("aaa"));
+        System.out.println(Hashing.md5().hashBytes("aaa".getBytes()).toString());
+      //System.out.println(SecureUtil.md5("aaa").substring(24));
       //System.out.println(Integer.valueOf(SecureUtil.md5("aaa").substring(24),16));//转换为10进制，取模：这样会转换失败，取模，可以只取后几位即可
       //System.out.println(Integer.valueOf(SecureUtil.md5("aaa"),16));
 
@@ -18,6 +21,11 @@ public class SecureUtilTest {
       //System.out.println(SecureUtil.md5(" aaa"));//两个md5值不同：..
       //System.out.println(Hashing.md5().hashBytes("aaa".getBytes()).toString());
       //System.out.println(Hashing.md5().hashBytes("aaa".getBytes()).toString());
+
+
+
+
+
     }
 
 }
