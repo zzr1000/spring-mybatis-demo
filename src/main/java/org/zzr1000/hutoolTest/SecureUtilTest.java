0c87e2e3fb965166a66340396d89d286a2c57dbf
zzr1000 <zzr1000@gmail.com>
2019-06-26 14:04:14 +0800

    进制转换

--- src/main/java/org/zzr1000/hutoolTest/SecureUtilTest.java
+++ src/main/java/org/zzr1000/hutoolTest/SecureUtilTest.java
@@ -9,8 +9,15 @@ public class SecureUtilTest {
     @Test
     public void test1() {
         System.out.println(SecureUtil.md5("aaa"));
+        System.out.println(SecureUtil.md5("aaa").substring(24));
+      //System.out.println(Integer.valueOf(SecureUtil.md5("aaa").substring(24),16));//转换为10进制，取模：这样会转换失败，取模，可以只取后几位即可
+      //System.out.println(Integer.valueOf(SecureUtil.md5("aaa"),16));
+
+        System.out.println(Integer.valueOf("444",16));
+
       //System.out.println(SecureUtil.md5(" aaa"));//两个md5值不同：..
-        System.out.println(Hashing.md5().hashBytes("aaa".getBytes()).toString());
+      //System.out.println(Hashing.md5().hashBytes("aaa".getBytes()).toString());
+      //System.out.println(Hashing.md5().hashBytes("aaa".getBytes()).toString());
     }
 
 }
