4ff9ceb98f77d2451362ffeffb9d46943abc5d95
zzr1000 <zzr1000@gmail.com>
2019-06-26 16:19:24 +0800

    cal

---
+++ src/main/java/org/zzr1000/typeTest/CalTest.java
@@ -0,0 +1,15 @@
+package org.zzr1000.typeTest;
+
+import org.junit.Test;
+
+public class CalTest {
+
+
+    @Test
+    public void test(){
+        System.out.println(8%10);
+        System.out.println(8/10);
+    }
+
+
+}
