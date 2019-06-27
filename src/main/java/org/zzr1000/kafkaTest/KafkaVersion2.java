3008f0ac74ef5d27a0d0d60afe4a6049629539c7
zzr1000 <zzr1000@gmail.com>
2019-06-27 20:00:56 +0800

    kafka

---
+++ src/main/java/org/zzr1000/kafkaTest/KafkaVersion2.java
@@ -0,0 +1,16 @@
+package org.zzr1000.kafkaTest;
+
+/*
+
+https://www.confluent.io/blog/
+https://www.oschina.net/search?scope=all&q=kafka+%E6%96%B0%E7%89%B9%E6%80%A7
+
+https://www.confluent.io/blog/apache-kafka-0-9-is-released/
+https://www.confluent.io/blog/announcing-apache-kafka-0-10-1-0/
+
+
+ */
+
+
+public class KafkaVersion2 {
+}
