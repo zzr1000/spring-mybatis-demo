d489840287363a0337e94b9e06078d9eb702ad57
zzr11000 <zzr1000@gmail.com>
2019-06-28 23:47:48 +0800

    Model pattern

--- src/main/java/org/zzr1000/mybatisTest/MybatisTest.java
+++ src/main/java/org/zzr1000/mybatisTest/MybatisTest.java
@@ -1,5 +1,11 @@
 package org.zzr1000.mybatisTest;
 
+/*
+
+refer to :
+https://github.com/wander-chu/spring-boot-mybatis
+
+ */
 public class MybatisTest {
 
     public static void main(String[] args) {
