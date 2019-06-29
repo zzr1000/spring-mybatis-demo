ae21761ce01e06afd3a18341432549f2d73201e1
zzr11000 <zzr1000@gmail.com>
2019-06-29 21:06:07 +0800

    httpclient

--- src/main/java/org/zzr1000/httpClientTest/HttpClientTest.java
+++ src/main/java/org/zzr1000/httpClientTest/HttpClientTest.java
@@ -1,4 +1,18 @@
 package org.zzr1000.httpClientTest;
 
+import cn.hutool.http.HttpUtil;
+import org.junit.Test;
+
 public class HttpClientTest {
+
+    @Test
+    public void test(){
+        String url = "http://localhost:8080/user/list/2";
+        String result1= HttpUtil.get(url);
+
+        //返回一个json：实现了接口调用：
+        //{"msg":"sucess","data":[{"id":2,"username":"BBBB","password":"bbb","age":20}],"state":1}
+        System.out.println(result1);
+    }
+
 }
