3c289869f92e7541ea58c403562e34699fa816ac
zzr11000 <zzr1000@gmail.com>
2019-06-30 23:19:36 +0800

    bytype byname

--- src/main/java/org/zzr1000/redisMysqlTest/RedisMysqlTest.java
+++ src/main/java/org/zzr1000/redisMysqlTest/RedisMysqlTest.java
@@ -2,6 +2,10 @@ package org.zzr1000.redisMysqlTest;
 
 public class RedisMysqlTest {
 
-
+    public static void main(String[] args) {
+        System.out.println("xxxx".hashCode());
+        System.out.println("yyyy".hashCode());
+        System.out.println("zzzz".hashCode());
+    }
 
 }
---
+++ src/main/java/org/zzr1000/sqlParse/SqlParse.java
@@ -0,0 +1,4 @@
+package org.zzr1000.sqlParse;
+
+public class SqlParse {
+}
