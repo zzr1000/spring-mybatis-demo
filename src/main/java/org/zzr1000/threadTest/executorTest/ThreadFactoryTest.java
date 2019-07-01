7e6265542c0d863edb33dca7ea20ddd633f12a67
zzr1000 <zzr1000@gmail.com>
2019-07-01 15:34:55 +0800

    thread

--- src/main/java/org/zzr1000/threadTest/executorTest/ThreadFactoryTest.java
+++ src/main/java/org/zzr1000/threadTest/executorTest/ThreadFactoryTest.java
@@ -4,6 +4,16 @@ package org.zzr1000.threadTest.executorTest;
 /**
  * refer to :
  * https://blog.csdn.net/chenchaofuck1/article/details/51589774
+ *
+ * 一句话总结：
+ * Executors中的每一个方法都是返回的Executor（ExecutorService），原因是：每个方法，都是通过new
+ * ThreadPoolExecutor 类返回的：
+ *
+ * 在new ThreadPoolExecutor时，可以指定ThreadFactory：
+ *
+ * 就是这个关系：. . .
+ *
+ *
  */
 public class ThreadFactoryTest {
 }
