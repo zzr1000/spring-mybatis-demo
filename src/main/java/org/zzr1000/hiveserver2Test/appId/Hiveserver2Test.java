069b717e54882ab385e818110aa8000e49a5d5b9
zzr1000 <zzr1000@gmail.com>
2019-07-02 19:15:34 +0800

    xml

--- src/main/java/org/zzr1000/hiveserver2Test/appId/Hiveserver2Test.java
+++ src/main/java/org/zzr1000/hiveserver2Test/appId/Hiveserver2Test.java
@@ -1,17 +1,18 @@
-package org.zzr1000.hiveserver2Test;
+package org.zzr1000.hiveserver2Test.appId;
 
 import org.apache.hive.jdbc.HiveStatement;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
 import java.util.Properties;
 
 
-public class Hiveserver2TestLog {
+public class Hiveserver2Test {
 
-    private static String driverName = "org.apache.hive.jdbc.HiveDriver";
+    private static String driverName =
+            "org.apache.hive.jdbc.HiveDriver";
 
     public static void main(String[] args) throws SQLException {
         try {
@@ -27,19 +28,25 @@ public class Hiveserver2TestLog {
         info.setProperty("password", "");
         info.setProperty("hiveconf:tez.queue.name", "vvv");
 
-        String JDBC_URL="jdbc:hive2://x.x.x.x:10000/c";
+        String JDBC_URL="jdbc:hive2://xx.xx.xx.xx:10000/c";
 
         Connection con = DriverManager.getConnection(
                 JDBC_URL, info);
-//      Statement stmt = con.createStatement();
+//        Statement stmt = con.createStatement();
         HiveStatement stmt = (HiveStatement) con.createStatement();
-
+//        String tableName = "wyphao";
+//        stmt.execute("drop table if exists " + tableName);
+//        stmt.execute("create table " + tableName +
+//                " (key int, value string)");
+//        System.out.println("Create table success!");
+//        // show tables
         String sql = "select student.no,people.no from student , people where student.no = people.no";
-
+//        System.out.println("Running: " + sql);
         ResultSet res = stmt.executeQuery(sql);
 //        if (res.next()) {
 //            System.out.println(res.getString(1));
 //        }
+
 
 //        String yarn_app_id = "";
 //        for (String log : stmt.getQueryLog()) {
@@ -49,6 +56,7 @@ public class Hiveserver2TestLog {
 //        }
 //        System.out.println("YARN Application ID: " + yarn_app_id);
 
+
         System.out.println("========begin:print job log========");
 
         for (String log : stmt.getQueryLog()) {
@@ -57,6 +65,28 @@ public class Hiveserver2TestLog {
 
         System.out.println("========end:print job log========");
 
+
+        // describe table
+//        sql = "describe " + tableName;
+//        System.out.println("Running: " + sql);
+//        res = stmt.executeQuery(sql);
+//        while (res.next()) {
+//            System.out.println(res.getString(1) + "\t" + res.getString(2));
+//        }
+//
+//
+//        sql = "select * from " + tableName;
+//        res = stmt.executeQuery(sql);
+//        while (res.next()) {
+//            System.out.println(String.valueOf(res.getInt(1)) + "\t"
+//                    + res.getString(2));
+//        }
+//
+//        sql = "select count(1) from " + tableName;
+//        System.out.println("Running: " + sql);
+//        res = stmt.executeQuery(sql);
+//        while (res.next()) {
+//            System.out.println(res.getString(1));
+//        }
     }
 }
-
--- src/main/java/org/zzr1000/hiveserver2Test/appId/Hiveserver2TestLog.java
+++ src/main/java/org/zzr1000/hiveserver2Test/appId/Hiveserver2TestLog.java
@@ -1,19 +1,17 @@
-package org.zzr1000.hiveserver2Test;
+package org.zzr1000.hiveserver2Test.appId;
 
 import org.apache.hive.jdbc.HiveStatement;
 
-import java.sql.SQLException;
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Properties;
 
 
-public class Hiveserver2Test {
+public class Hiveserver2TestLog {
 
-    private static String driverName =
-            "org.apache.hive.jdbc.HiveDriver";
+    private static String driverName = "org.apache.hive.jdbc.HiveDriver";
 
     public static void main(String[] args) throws SQLException {
         try {
@@ -29,25 +27,19 @@ public class Hiveserver2Test {
         info.setProperty("password", "");
         info.setProperty("hiveconf:tez.queue.name", "vvv");
 
-        String JDBC_URL="jdbc:hive2://xx.xx.xx.xx:10000/c";
+        String JDBC_URL="jdbc:hive2://x.x.x.x:10000/c";
 
         Connection con = DriverManager.getConnection(
                 JDBC_URL, info);
-//        Statement stmt = con.createStatement();
+//      Statement stmt = con.createStatement();
         HiveStatement stmt = (HiveStatement) con.createStatement();
-//        String tableName = "wyphao";
-//        stmt.execute("drop table if exists " + tableName);
-//        stmt.execute("create table " + tableName +
-//                " (key int, value string)");
-//        System.out.println("Create table success!");
-//        // show tables
+
         String sql = "select student.no,people.no from student , people where student.no = people.no";
-//        System.out.println("Running: " + sql);
+
         ResultSet res = stmt.executeQuery(sql);
 //        if (res.next()) {
 //            System.out.println(res.getString(1));
 //        }
-
 
 //        String yarn_app_id = "";
 //        for (String log : stmt.getQueryLog()) {
@@ -57,7 +49,6 @@ public class Hiveserver2Test {
 //        }
 //        System.out.println("YARN Application ID: " + yarn_app_id);
 
-
         System.out.println("========begin:print job log========");
 
         for (String log : stmt.getQueryLog()) {
@@ -66,28 +57,6 @@ public class Hiveserver2Test {
 
         System.out.println("========end:print job log========");
 
-
-        // describe table
-//        sql = "describe " + tableName;
-//        System.out.println("Running: " + sql);
-//        res = stmt.executeQuery(sql);
-//        while (res.next()) {
-//            System.out.println(res.getString(1) + "\t" + res.getString(2));
-//        }
-//
-//
-//        sql = "select * from " + tableName;
-//        res = stmt.executeQuery(sql);
-//        while (res.next()) {
-//            System.out.println(String.valueOf(res.getInt(1)) + "\t"
-//                    + res.getString(2));
-//        }
-//
-//        sql = "select count(1) from " + tableName;
-//        System.out.println("Running: " + sql);
-//        res = stmt.executeQuery(sql);
-//        while (res.next()) {
-//            System.out.println(res.getString(1));
-//        }
     }
 }
+
