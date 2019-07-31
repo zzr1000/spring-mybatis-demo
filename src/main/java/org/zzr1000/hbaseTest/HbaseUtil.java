fcd42f2f23f5d56797b48a0da82973bc715d9bb3
zzr1000 <zzr1000@gmail.com>
2019-07-31 20:11:42 +0800

    ci

--- src/main/java/org/zzr1000/hbaseTest/HbaseUtil.java
+++ src/main/java/org/zzr1000/hbaseTest/HbaseUtil.java
@@ -2,6 +2,7 @@ package org.zzr1000.hbaseTest;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
@@ -51,6 +52,25 @@ public class HbaseUtil {
         b = rs.isEmpty();
         closeConnection();
         return !b;
+    }
+
+
+
+    //QueryByRowkey
+    public static void QueryByRK(String tableName , String rowKey) throws IOException {
+
+        getConnection();
+        Table table = hbaseConnection.getTable(TableName.valueOf(tableName));
+        Get get = new Get(rowKey.getBytes());
+        Result r = table.get(get);
+
+        if(!r.isEmpty()) {
+            System.out.println("rowkey:" + new String(r.getRow()));
+            for (KeyValue keyValue : r.raw()) {
+                System.out.println("列：" + new String(keyValue.getFamily())
+                        + "====值:" + new String(keyValue.getValue()));
+            }
+        }
     }
 
 
